c838edab00e8f050d69821f3e66b147e801a0331
Michael <michaelfarquhar4@gmail.com>
2022-11-22 20:15:17 -0500

    Add quote card that displays random quote from api

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,38 @@
 <script setup>
-import { onMounted } from "vue";
+import axios from "axios";
+import { onMounted, ref } from "vue";
 import { themeChange } from "theme-change";
 
 import TheGithubLink from "./components/TheGithubLink.vue";
 import TheThemeSelect from "./components/TheThemeSelect.vue";
+import TheQuoteDisplay from "./components/TheQuoteDisplay.vue";
+
+let quote = ref({});
+const isLoading = ref(true);
 
 onMounted(() => {
   themeChange(false);
+
+  // Get quote from API
+  axios.get("https://api.quotable.io/random").then((resp) => {
+    quote.value = resp.data;
+    isLoading.value = false;
+  });
 });
 </script>
 
 <template>
-  <main>
+  <main class="app bg-base-200">
     <TheThemeSelect />
-    <h1>Test</h1>
+    <div className="card bg-base-100 shadow-xl w-10/12 lg:w-6/12">
+      <div className="card-body text-center">
+        <TheQuoteDisplay
+          :content="quote.content"
+          :author="quote.author"
+          :isLoading="isLoading"
+        />
+      </div>
+    </div>
     <TheGithubLink />
   </main>
 </template>
---
+++ src/components/TheQuoteDisplay.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+  content: String,
+  author: String,
+  isLoading: Boolean,
+});
+</script>
+
+<template>
+  <button v-if="isLoading" class="btn btn-ghost loading"></button>
+  <h2 v-if="!isLoading" className="card-title justify-center">{{ content }}</h2>
+  <p v-if="!isLoading" className="italic">- {{ author }}</p>
+</template>
